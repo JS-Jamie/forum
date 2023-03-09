7592df40c14c74e3277ae67dbb162b4bf858f206
JS-Jamie <jamie.jeesoo.shin@gmail.com>
2023-03-09 00:16:44 -0800

    Generate an array of forumPost components

--- src/forumMain/ForumMain.jsx
+++ src/forumMain/ForumMain.jsx
@@ -1,33 +1,52 @@
 import React from 'react';
 import { Container } from 'react-dom';
 
-const ForumMain = () => {
-  const samplePosts = [
-    {
-      title: 'The Best Articles for Learning Something New',
-      author: 'johndoe',
-      comments: 52,
-      likes: 1200,
-    },
-    {
-      title: 'I Finally Finished My First JavaScript Project!',
-      author: 'janesmith',
-      comments: 27,
-      likes: 650,
-    },
-    {
-      title: "What's the Best Way to Learn CSS?",
-      author: 'bobross',
-      comments: 83,
-      likes: 2100,
-    },
-  ];
+const samplePosts = [
+  {
+    title: 'The Best Articles for Learning Something New',
+    author: 'johndoe',
+    comments: 52,
+    likes: 1200,
+  },
+  {
+    title: 'I Finally Finished My First JavaScript Project!',
+    author: 'janesmith',
+    comments: 27,
+    likes: 650,
+  },
+  {
+    title: "What's the Best Way to Learn CSS?",
+    author: 'bobross',
+    comments: 83,
+    likes: 2100,
+  },
+];
+function forumPost({ title, author, comments, likes }) {
   return (
-    <Container>
-      <h1>Community Forum</h1>
-      <div></div>
-    </Container>
+    <div className='post'>
+      <h2>{title}</h2>
+      <p>Submitted by {author}</p>
+      <p>
+        {comments} comments | {likes} likes
+      </p>
+    </div>
   );
-};
+}
 
-export default ForumMain;
+function forumPostList() {
+  return (
+    <div className='post-list'>
+      {samplePosts.map((post) => (
+        <forumPost
+          key={post.title}
+          title={post.title}
+          author={post.author}
+          comments={post.comments}
+          hearts={post.hearts}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default forumPostList;
